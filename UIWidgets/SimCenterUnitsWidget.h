216561254ecfb06fe9a32564d83e8e5ed358e100
Stevan Gavrilovic <gavrilovic@berkeley.edu>
2021-10-20 13:42:24 -0700

    Add missing SimCenterUnitWidget classes

--- UIWidgets/SimCenterUnitsWidget.h
+++ UIWidgets/SimCenterUnitsWidget.h
@@ -1,5 +1,5 @@
-#ifndef UserInputGMWidget_H
-#define UserInputGMWidget_H
+#ifndef SIMCENTERUNITSWIDGET_H
+#define SIMCENTERUNITSWIDGET_H
 /* *****************************************************************************
 Copyright (c) 2016-2021, The Regents of the University of California (Regents).
 All rights reserved.
@@ -19,7 +19,7 @@ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
 DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
 ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
@@ -38,84 +38,31 @@ UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
 
 // Written by: Stevan Gavrilovic, Frank McKenna
 
-#include "GroundMotionStation.h"
-#include "SimCenterAppWidget.h"
+class SimCenterUnitsCombo;
+class QGridLayout;
 
-#include <memory>
+#include <QGroupBox>
+#include "JsonSerializable.h"
 
-#include <QMap>
-
-class VisualizationWidget;
-
-class QStackedWidget;
-class QLineEdit;
-class QProgressBar;
-class QLabel;
-
-namespace Esri
+class SimCenterUnitsWidget : public QGroupBox, public JsonSerializable
 {
-namespace ArcGISRuntime
-{
-class ArcGISMapImageLayer;
-class GroupLayer;
-class FeatureCollectionLayer;
-class KmlLayer;
-class Layer;
-}
-}
-
-
-class UserInputGMWidget : public SimCenterAppWidget
-{
-    Q_OBJECT
-
 public:
-    UserInputGMWidget(VisualizationWidget* visWidget, QWidget *parent = nullptr);
-    ~UserInputGMWidget();
+    SimCenterUnitsWidget(QString title = "Event Units",QWidget* parent = nullptr);
 
-    void showUserGMLayers(bool state);
+    bool outputToJSON(QJsonObject &jsonObject);
 
-    QStackedWidget* getUserInputGMWidget(void);
+    bool inputFromJSON(QJsonObject &jsonObject);
 
-    bool outputToJSON(QJsonObject &jsonObj);
-    bool inputAppDataFromJSON(QJsonObject &jsonObj);
-    bool outputAppDataToJSON(QJsonObject &jsonObj);
+    void reset(void);
 
     void clear(void);
 
-public slots:
-
-    void showUserGMSelectDialog(void);
-
-private slots:
-
-    void loadUserGMData(void);
-    void chooseEventFileDialog(void);
-    void chooseMotionDirDialog(void);
-
-signals:
-    void outputDirectoryPathChanged(QString motionDir, QString eventFile);
-    void loadingComplete(const bool value);
+    void addNewUnitItem(const QString& name);
 
 private:
+    QGridLayout *unitsLayout = nullptr;
 
-    std::unique_ptr<QStackedWidget> userGMStackedWidget;
-
-    VisualizationWidget* theVisualizationWidget;
-
-    QString eventFile;
-    QString motionDir;
-
-    QLineEdit *eventFileLineEdit;
-    QLineEdit *motionDirLineEdit;
-
-    QLabel* progressLabel;
-    QWidget* progressBarWidget;
-    QWidget* fileInputWidget;
-    QProgressBar* progressBar;
-
-    QVector<GroundMotionStation> stationList;
-
+    SimCenterUnitsCombo* findChild(const QString& name);
 };
 
-#endif // UserInputGMWidget_H
+#endif // SIMCENTERUNITSWIDGET_H
