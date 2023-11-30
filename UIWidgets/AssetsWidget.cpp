7fd7185e2bf7a284685e068d2663a2ec8da96d1b
jinyan1214 <jinyan_zhao@berkeley.edu>
2023-11-30 14:14:55 -0800

    comment out roadway segmentation

--- UIWidgets/AssetsWidget.cpp
+++ UIWidgets/AssetsWidget.cpp
@@ -108,16 +108,16 @@ AssetsWidget::AssetsWidget(QWidget *parent, VisualizationWidget* visWidget)
     transportNetworkWidget->addComponent(QString("Json to Transportation Network AIM"), QString("JSON_to_TRANSPORTNETWORK"), jsonTransportNetworkInventory);
 
     GeojsonAssetInputWidget *GeoJsonTransportNetworkAssetInventory = new GeojsonAssetInputWidget(this,visualizationWidget,"Transportation Network","GEOJSON_TO_ASSET");
-    QWidget* roadLengthWidget = new QWidget();
-    QHBoxLayout* roadLengthLayout = new QHBoxLayout(roadLengthWidget);
-    QLabel* roadLengthLabel = new QLabel("Maximum roadway length (m) per AIM",this);
-    QLineEdit* roadLengthLineEdit = new QLineEdit(this);
-    roadLengthLineEdit->setText("100.0");
-    QDoubleValidator *validator = new QDoubleValidator(this);
-    validator->setBottom(0.0);
-    roadLengthLayout->addWidget(roadLengthLabel);
-    roadLengthLayout->addWidget(roadLengthLineEdit);
-    GeoJsonTransportNetworkAssetInventory->insertLineEditToMainLayout(1, roadLengthWidget, roadLengthLineEdit, "Roadway", "maxRoadLength_m");
+//    QWidget* roadLengthWidget = new QWidget();
+//    QHBoxLayout* roadLengthLayout = new QHBoxLayout(roadLengthWidget);
+//    QLabel* roadLengthLabel = new QLabel("Maximum roadway length (m) per AIM",this);
+//    QLineEdit* roadLengthLineEdit = new QLineEdit(this);
+//    roadLengthLineEdit->setText("100.0");
+//    QDoubleValidator *validator = new QDoubleValidator(this);
+//    validator->setBottom(0.0);
+//    roadLengthLayout->addWidget(roadLengthLabel);
+//    roadLengthLayout->addWidget(roadLengthLineEdit);
+//    GeoJsonTransportNetworkAssetInventory->insertLineEditToMainLayout(1, roadLengthWidget, roadLengthLineEdit, "Roadway", "maxRoadLength_m");
 
     transportNetworkWidget->addComponent(QString("GeoJSON to Asset"), QString("GEOJSON_TO_ASSET"), GeoJsonTransportNetworkAssetInventory);
 
