8cac56a303d7fe39cc4824c9b5d07a34129e50fc
fmckenna <fmckenna@berkeley.edu>
2025-02-04 00:32:58 -0800

    fmk - updating main.cpp to 5.3.0

--- main.cpp
+++ main.cpp
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
     // Setting Core Application Name, Organization, Version
     QCoreApplication::setApplicationName("R2D");
     QCoreApplication::setOrganizationName("SimCenter");
-    QCoreApplication::setApplicationVersion("5.2.0");
+    QCoreApplication::setApplicationVersion("5.3.0");
 
     // set up logging of output messages for user debugging
     logFilePath = QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation)
